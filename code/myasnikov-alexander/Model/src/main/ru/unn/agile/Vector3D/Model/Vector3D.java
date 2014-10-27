0aee09db600a962581bf3fd77f778b70d5eb4d0f
Alexander.Myasnikov <myasnikov.alexander.s@gmail.com>
2014-10-27 22:42:17 +0300

    added function GetNorm()

--- code/myasnikov-alexander/Model/src/main/ru/unn/agile/Vector3D/Model/Vector3D.java
+++ code/myasnikov-alexander/Model/src/main/ru/unn/agile/Vector3D/Model/Vector3D.java
@@ -1,8 +1,20 @@
 package ru.unn.agile.Vector3D.model;
 
+
+
 public class Vector3D {
 
-    public Vector3D() {
+    double x;
+    double y;
+    double z;
 
+    public Vector3D(double value1, double value2, double value3) {
+        x = value1;
+        y = value2;
+        z = value3;
+    }
+
+    public double GetNorm() {
+        return Math.sqrt(x*x + y*y + z*z);
     }
 }
--- code/myasnikov-alexander/Model/src/test/ru/unn/agile/Vector3D/Model/Vector3DTest.java
+++ code/myasnikov-alexander/Model/src/test/ru/unn/agile/Vector3D/Model/Vector3DTest.java
@@ -5,9 +5,32 @@ import static org.junit.Assert.*;
 
 public class Vector3DTest {
 
+    private double delta=0;
+
     @Test
-    public void t() {
-        assertEquals(0,0);
+    public void canGetNormDouble() {
+        Vector3D vector = new Vector3D( 1.0, 0.0, 0.0 );
+
+        double result = vector.GetNorm();
+
+        assertEquals(result, 1, delta);
     }
 
+    @Test
+    public void canGetNormInt() {
+        Vector3D vector = new Vector3D( 3, 0, 4 );
+
+        double result = vector.GetNorm();
+
+        assertEquals(result, 5.0, delta);
+    }
+
+    @Test
+    public void canGetNormNegativeNumbers() {
+        Vector3D vector = new Vector3D( 0, -3, 0 );
+
+        double result = vector.GetNorm();
+
+        assertEquals(result,3.0 , delta);
+    }
 }

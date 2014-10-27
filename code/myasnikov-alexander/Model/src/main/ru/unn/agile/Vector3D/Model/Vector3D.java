474bbb121cb1718906afb71f7fcc3d46469327bf
Alexander.Myasnikov <myasnikov.alexander.s@gmail.com>
2014-10-27 19:26:34 +0300

    Created template Vector3D

---
+++ code/myasnikov-alexander/Model/src/main/ru/unn/agile/Vector3D/Model/Vector3D.java
@@ -0,0 +1,8 @@
+package ru.unn.agile.Vector3D.model;
+
+public class Vector3D {
+
+    public Vector3D() {
+
+    }
+}
---
+++ code/myasnikov-alexander/Model/src/test/ru/unn/agile/Vector3D/Model/Vector3DTest.java
@@ -0,0 +1,13 @@
+package ru.unn.agile.Vector3D.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Vector3DTest {
+
+    @Test
+    public void t() {
+        assertEquals(0,0);
+    }
+
+}

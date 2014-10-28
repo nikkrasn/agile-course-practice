bd2d1e0a33b7964b210bb1da906c67acb53b9e22
ryabenko <a.bure999@yandex.ru>
2014-10-28 07:49:39 +0400

    Refactoring the code

--- code/ryabenko-sergey/Model/src/test/ru/unn/agile/Matrix/MatrixDeterminantTest.java
+++ code/ryabenko-sergey/Model/src/test/ru/unn/agile/Matrix/MatrixDeterminantTest.java
@@ -10,55 +10,48 @@ public class MatrixDeterminantTest {
 
     @Test (expected = IllegalArgumentException.class)
     public void canCheckMatrixIsEmpty() {
-        double[][] d = {{}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{}});
         double det = MatrixDeterminant.determinant(mat);
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void canCheckMatrixIsSquare() {
-        double[][] d = {{-3.55, 4, 1}, {1, 0, 0}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{-3.55, 4, 1}, {1, 0, 0}});
         double det = MatrixDeterminant.determinant(mat);
     }
 
     @Test
     public void canFindDeterminantForFirstOrderMatrix() {
-        double[][] d = {{-3.55}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{-3.55}});
         double det = MatrixDeterminant.determinant(mat);
         assertEquals(-3.55, det, delta);
     }
 
     @Test
     public void canFindDeterminantForSecondOrderMatrix() {
-        double[][] d = {{1.2, -1}, {-0.25, 2.4}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{1.2, -1}, {-0.25, 2.4}});
         double det = MatrixDeterminant.determinant(mat);
         assertEquals(1.2 * 2.4 + 1 * -0.25, det, delta);
     }
 
     @Test
     public void canFindDeterminantForHigherOrderMatrix() {
-        double[][] d = {{1, 2, 3, 4}, {1, -2, 3, -4},
-                {1.1, 1.2, 1.3, 1.4}, {2.21, 3.31, 4.41, 1.01}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{1, 2, 3, 4}, {1, -2, 3, -4},
+                {1.1, 1.2, 1.3, 1.4}, {2.21, 3.31, 4.41, 1.01}});
         double det = MatrixDeterminant.determinant(mat);
         assertEquals(-36, det, delta);
     }
 
     @Test
     public void canFindDeterminantForIncorrectlyEnteredMatrix() {
-        double[][] d = {{1, 2, 3}, {4, 5}, {6}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{1, 2, 3}, {4, 5}, {6}});
         double det = MatrixDeterminant.determinant(mat);
         assertEquals(-90, det, delta);
     }
 
     @Test
     public void canFindDeterminantForHalfEmptyMatrix() {
-        double[][] d = {{}, {}, {1, 0, 1, 0, 1}, {}, {}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{}, {}, {1, 0, 1, 0, 1}, {}, {}});
         double det = MatrixDeterminant.determinant(mat);
         assertEquals(0, det, delta);
     }
@@ -66,11 +59,8 @@ public class MatrixDeterminantTest {
     @Test
      public void canCreateSubMatrix() {
 
-        double[][] d = {{1, 0, 0}, {0, 1, 1}, {0, 1, 6}};
-        double[][] shortD = {{1, 1}, {1, 6}};
-
-        Matrix mat = new Matrix(d);
-        Matrix shortMatrix = new Matrix(shortD);
+        Matrix mat = new Matrix(new double[][]{{1, 0, 0}, {0, 1, 1}, {0, 1, 6}});
+        Matrix shortMatrix = new Matrix(new double[][]{{1, 1}, {1, 6}});
 
         Matrix subMat = MatrixDeterminant.createSubMatrix(mat, 0, 0);
 
@@ -83,16 +73,14 @@ public class MatrixDeterminantTest {
 
     @Test
     public void canCreateFirstOrderSubMatrix() {
-        double[][] d = {{1, 2}, {3, -1}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{1, 2}, {3, -1}});
         Matrix subMat = MatrixDeterminant.createSubMatrix(mat, 0, 1);
         assertTrue(subMat.getValueAt(0, 0) == 3 && subMat.getSize() == 1);
     }
 
     @Test
     public void canCreateEmptiesSubMatrix() {
-        double[][] d = {{-10}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{-10}});
         Matrix subMat = MatrixDeterminant.createSubMatrix(mat, 0, 0);
         assertTrue(subMat.getCountRows() == 0);
     }
--- code/ryabenko-sergey/Model/src/test/ru/unn/agile/Matrix/MatrixTest.java
+++ code/ryabenko-sergey/Model/src/test/ru/unn/agile/Matrix/MatrixTest.java
@@ -26,29 +26,25 @@ public class MatrixTest {
 
     @Test
     public void canFindRowCountInMatrix() {
-        double[][] d = {{0.5, -2, 8.7}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{0.5, -2, 8.7}});
         assertEquals(mat.getCountRows(), 1);
     }
 
     @Test
     public void canFindColumnCountInMatrix() {
-        double[][] d = {{0.5, -2, 8.7}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{0.5, -2, 8.7}});
         assertEquals(3, mat.getCountColumns());
     }
 
     @Test
     public void canFindMatrixDimensionWithIncorrectlyInput() {
-        double[][] d = {{}, {4, 5, 7, 8, 8, 8}, {1}, {1, 1, 1}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{}, {4, 5, 7, 8, 8, 8}, {1}, {1, 1, 1}});
         assertTrue(mat.getCountRows() == 4 && mat.getCountColumns() == 6);
     }
 
     @Test
     public void canSetInitialMatrixElement() {
-        double[][] d = {{0.5, -2, 8.7}, {0, 8, 1}, {76.32, -0.55, 0}};
-        Matrix mat = new Matrix(d);
+        Matrix mat = new Matrix(new double[][]{{0.5, -2, 8.7}, {0, 8, 1}, {76.32, -0.55, 0}});
         assertEquals(-0.55, mat.getValueAt(2, 1), delta);
     }
 }

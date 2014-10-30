bc3ffaed65c79d12808445a219cfa8f33db1b327
EgorSharov <egershar@me.com>
2014-10-31 01:38:44 +0300

    Исправил Spelling, добавил гетеры и сетеры,а так же тесты

---
+++ code/Sharov-Egor/Model/src/main/ru/unn/agile/ComplexProcent/Model/ComplexDeposit.java
@@ -0,0 +1,46 @@
+package ru.unn.agile.ComplexProcent.Model;
+
+public class ComplexDeposit {
+    private double base;
+    private double percent;
+    private double interestCountInYear;
+
+    public ComplexDeposit(final double depositBase, final double depositProcent, final double depositInterestCount){
+        this.setBase(depositBase);
+        this.setPercent(depositProcent);
+        this.setInterestCountInYear(depositInterestCount);
+    }
+
+    public double getCapitalizedBase(int years){
+        return this.getBase() *Math.pow((1 + (this.getPercent() / this.getInterestCountInYear())), years * this.getInterestCountInYear());
+    };
+
+
+    public double getBase() {
+        return base;
+    }
+
+    public ComplexDeposit setBase(double base) {
+        this.base = base;
+        return  this;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public ComplexDeposit setPercent(double percent) {
+        this.percent = percent /100;
+        return this;
+    }
+
+    public double getInterestCountInYear() {
+        return interestCountInYear;
+    }
+
+
+    public ComplexDeposit setInterestCountInYear(double interestCountInYear) {
+        this.interestCountInYear = interestCountInYear;
+        return this;
+    }
+}

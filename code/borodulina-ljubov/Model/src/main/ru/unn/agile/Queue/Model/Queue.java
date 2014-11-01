86a20e8165d8b9b31076a184c993f50fc24ed789
Borodulina Ljubov <borodulinals@gmail.com>
2014-11-01 23:36:38 +0300

    queue: add remove() method

--- code/borodulina-ljubov/Model/src/main/ru/unn/agile/Queue/Model/Queue.java
+++ code/borodulina-ljubov/Model/src/main/ru/unn/agile/Queue/Model/Queue.java
@@ -1,17 +1,46 @@
 package ru.unn.agile.Queue.Model;
 
 public class Queue<T> {
-    private T first;
+    private Node first;
+
+    private class Node {
+        private T    item;
+        private Node next;
+    }
 
     public boolean isEmpty() {
         return first == null;
     }
 
     public void add(T item) {
-        first = item;
+        Node newLast = new Node();
+        newLast.item = item;
+        newLast.next = null;
+
+        if (isEmpty()) {
+            first = newLast;
+        }
+        else {
+            Node last = getLast();
+            last.next = newLast;
+        }
+    }
+
+    private Node getLast() {
+        Node temp = first;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public T remove() {
+        T itemToReturn = first.item;
+        first = first.next;
+        return itemToReturn;
     }
 
     public T element() {
-        return first;
+        return first.item;
     }
 }
--- code/borodulina-ljubov/Model/src/test/ru/unn/agile/Queue/Model/QueueTest.java
+++ code/borodulina-ljubov/Model/src/test/ru/unn/agile/Queue/Model/QueueTest.java
@@ -2,28 +2,72 @@ package ru.unn.agile.Queue.Model;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QueueTest {
+    private Queue<Integer> queue;
+
+    @Before
+    public void setUp() {
+        queue = new Queue<Integer>();
+    }
+
+    @After
+    public void tearDown() {
+        queue = null;
+    }
+
     @Test
     public void isNewlyCreatedQueueEmpty() {
-        Queue queue = new Queue();
         assertTrue(queue.isEmpty());
     }
 
     @Test
     public void isQueueNotEmptyAfterAdd() {
-        Queue<Integer> queue = new Queue<Integer>();
         queue.add(1);
         assertFalse(queue.isEmpty());
     }
 
     @Test
     public void canAddItem() {
-        Queue<Integer> queue = new Queue<Integer>();
         Integer itemToAdd = 1;
+
         queue.add(itemToAdd);
+
         assertEquals(itemToAdd, queue.element());
     }
 
-    
+    @Test
+    public void canAddTwoItems() {
+        List<Integer> itemsToAdd     = getIncrementalList(2);
+        List<Integer> itemsFromQueue = getEmptyList();
+        for (int item: itemsToAdd) {
+            queue.add(item);
+        }
+
+        while (!queue.isEmpty()) {
+            itemsFromQueue.add(queue.remove());
+        }
+
+        assertTrue(itemsToAdd.equals(itemsFromQueue));
+    }
+
+    private ArrayList<Integer> getEmptyList() {
+        return new ArrayList<Integer>();
+    }
+
+    private List<Integer> getIncrementalList(final int count) {
+        List<Integer> items = getEmptyList();
+        fillInListIncrementally(items, count);
+        return items;
+    }
+
+    private void fillInListIncrementally(final List<Integer> list, final int count) {
+        for (int i = 1; i <= count; ++i) {
+            list.add(i);
+        }
+    }
 }

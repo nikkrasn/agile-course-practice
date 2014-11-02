98f007d78dc0351ba8367ed6ccfcb54b2dcf6182
bysheva <bysheva.tatyana@gmail.com>
2014-11-02 22:23:34 +0300

    the first test and method for L2 metric were created

--- code/bysheva-tatyana/Model/src/main/ru/unn/agile/Metrics/Model/Metrics.java
+++ code/bysheva-tatyana/Model/src/main/ru/unn/agile/Metrics/Model/Metrics.java
@@ -13,4 +13,14 @@ public class Metrics {
 
         return metricL1;
     }
+
+    public static float L2(Vector<Float> vector1, Vector<Float> vector2) {
+        float metricL2 = 0.0f;
+
+        for (int i = 0; i < vector1.size(); i++) {
+            metricL2 += Math.sqrt(Math.pow(vector1.elementAt(i)-vector2.elementAt(i), 2));
+        }
+
+        return metricL2;
+    }
 }
--- code/bysheva-tatyana/Model/src/test/ru/unn/agile/Metrics/Model/TestMetrics.java
+++ code/bysheva-tatyana/Model/src/test/ru/unn/agile/Metrics/Model/TestMetrics.java
@@ -9,6 +9,9 @@ import java.util.Vector;
 import static org.junit.Assert.*;
 
 public class TestMetrics {
+
+    public final float epsilon = 0.01f;
+
     @Test
     public void checkL1MetricForOneDimensionVectors() {
         Float[] array = new Float[]{0.0f};
@@ -18,6 +21,18 @@ public class TestMetrics {
 
         float metricL1 = Metrics.L1(vector1, vector2);
 
-        assertTrue(metricL1 == 0.0f);
+        assertEquals(metricL1, 0.0f, epsilon);
+    }
+
+    @Test
+    public void checkL2MetricForOneDimensionVectors() {
+        Float[] array = new Float[]{0.0f};
+
+        Vector<Float> vector1 = new Vector<Float>(Arrays.asList(array));
+        Vector<Float> vector2 = new Vector<Float>(Arrays.asList(array));
+
+        float metricL2 = Metrics.L2(vector1, vector2);
+
+        assertEquals(metricL2, 0.0f, epsilon);
     }
 }

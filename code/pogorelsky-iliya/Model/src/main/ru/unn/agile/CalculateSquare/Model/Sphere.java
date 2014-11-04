f5d0cc032574d86df7d98fceb81e917fbf32e6c4
PogorelskyIliya <redvix1993@gmail.com>
2014-11-05 00:15:50 +0300

    Update Sphere.java

--- code/pogorelsky-iliya/Model/src/main/ru/unn/agile/CalculateSquare/Model/Sphere.java
+++ code/pogorelsky-iliya/Model/src/main/ru/unn/agile/CalculateSquare/Model/Sphere.java
@@ -3,13 +3,13 @@ package ru.unn.agile.CalculateSquare.Model;
 public class Sphere {
 
     private final double radius;
-    private static final int CONSTANTA = 4;
+    private static final int COEFF_SPHERE = 4;
 
     public Sphere(final double radius) {
         this.radius = radius;
     }
 
     public double calculateSquare() {
-        return CONSTANTA * Math.PI * Math.pow(radius, 2);
+        return COEFF_SPHERE * Math.PI * Math.pow(radius, 2);
     }
 }

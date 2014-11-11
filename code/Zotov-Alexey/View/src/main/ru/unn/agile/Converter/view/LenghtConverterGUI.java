d5d20bc5692b6710ab9728e0070626b7b9eefe6c
AlexeyZotov85m21 <alexeyzotov93@gmail.com>
2014-11-11 23:25:09 +0300

    travis

--- code/Zotov-Alexey/View/src/main/ru/unn/agile/Converter/view/LenghtConverterGUI.java
+++ code/Zotov-Alexey/View/src/main/ru/unn/agile/Converter/view/LenghtConverterGUI.java
@@ -6,13 +6,7 @@ import javax.swing.*;
  * Created by Алексей on 11.11.2014.
  */
 public class LenghtConverterGUI {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("LenghtConverterGUI");
-        frame.setContentPane(new LenghtConverterGUI().mainPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
 
-    private JPanel mainPanel;
+
+   // private JPanel mainPanel;
 }

49c76c3c12eae9f1949737d48a24f0166034500c
Daniil Osokin <daniil.osokin@gmail.com>
2014-11-15 13:34:59 +0300

    Fixed lab

--- code/blinkova-ekaterina/Model/src/test/java/ru/unn/agile/TemperatureConverter/Model/TemperatureConverterTest.java
+++ code/blinkova-ekaterina/Model/src/test/java/ru/unn/agile/TemperatureConverter/Model/TemperatureConverterTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 public class TemperatureConverterTest {
     private final double delta = 0.001;
-    
+
     @Test
     public void celsiusToFahrenheitZero() {
         TemperatureConverter convertation = createTemperatureConverter();

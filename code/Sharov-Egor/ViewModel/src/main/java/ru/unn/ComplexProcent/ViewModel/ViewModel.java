e089a4058389e64baa16aef3ac3ca7388cf5ad6b
EgorSharov <egershar@me.com>
2014-11-21 00:52:09 +0300

    started

---
+++ code/Sharov-Egor/ViewModel/src/main/java/ru/unn/ComplexProcent/ViewModel/ViewModel.java
@@ -0,0 +1,20 @@
+package ru.unn.ComplexProcent.ViewModel;
+
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewModel {
+    private final StringProperty re1 = new SimpleStringProperty();
+    private final StringProperty im1 = new SimpleStringProperty();
+    private final StringProperty re2 = new SimpleStringProperty();
+    private final StringProperty im2 = new SimpleStringProperty();
+
+
+}

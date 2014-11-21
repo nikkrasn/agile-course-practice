faee33f8c23a4bd0a7f84bf67dfc7e200ccfa87c
aryabush <annadmitrja@yandex.ru>
2014-11-21 13:15:58 +0300

    Added VIEW

---
+++ code/Ryabusheva-Anna/View/src/main/java/ru/unn/agile/DeterminatorIntersection/view/Determinator.java
@@ -0,0 +1,38 @@
+package ru.unn.agile.DeterminatorIntersection.view;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class Determinator {
+
+    @FXML
+    private TextField txtPlaneA;
+    @FXML
+    private TextField txtPlaneB;
+    @FXML
+    private TextField txtPlaneC;
+    @FXML
+    private TextField txtPlaneD;
+    @FXML
+    private TextField txtLinePointX;
+    @FXML
+    private TextField txtLinePointY;
+    @FXML
+    private TextField txtLinePointZ;
+    @FXML
+    private TextField txtLineVectorX;
+    @FXML
+    private TextField txtLineVectorY;
+    @FXML
+    private TextField txtLineVectorZ;
+    @FXML
+    private Button btnDet;
+
+    @FXML
+    void initialize() {
+    }
+}

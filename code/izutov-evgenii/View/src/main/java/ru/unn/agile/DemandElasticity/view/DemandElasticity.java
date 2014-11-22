fc536bb7c6bf53493f7875de23f032bbe232cb54
Evgenii Izutov <izutov.e.o@gmail.com>
2014-11-22 21:16:26 +0300

    Added layout of View.

---
+++ code/izutov-evgenii/View/src/main/java/ru/unn/agile/DemandElasticity/view/DemandElasticity.java
@@ -0,0 +1,13 @@
+package ru.unn.agile.DemandElasticity.view;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+//import ru.unn.agile.DemandElasticity.viewmodel.ViewModel;
+
+public class DemandElasticity {
+
+}

441a308392297da138341ca73484bd380ad2fd53
Evgenii Izutov <izutov.e.o@gmail.com>
2014-11-22 21:16:26 +0300

    Added layout of View.

---
+++ code/izutov-evgenii/View/src/main/java/ru/unn/agile/DemandElasticity/view/Main.java
@@ -0,0 +1,22 @@
+package ru.unn.agile.DemandElasticity.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("DemandElasticity.fxml"));
+        primaryStage.setTitle("Demand elasticity");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+}

e9db8e48fa5d585aef8f616c580d6e861b306c84
BlinkovaEkaterina <k-06-11@mail.ru>
2014-11-23 17:21:45 +0300

    Added ViewModel

---
+++ code/blinkova-ekaterina/ViewModel/src/main/java/ru/unn/agile/TemperatureConverter/viewmodel/ViewModel.java
@@ -0,0 +1,128 @@
+package ru.unn.agile.TemperatureConverter.viewmodel;
+
+
+import ru.unn.agile.TemperatureConverter.Model.TemperatureConverter;
+
+public class ViewModel {
+    private String inputValue;
+    private String result;
+    private String status;
+    private Scale scale;
+    private boolean isConvertButtonEnable;
+
+    public ViewModel() {
+        inputValue = "";
+        result = "";
+        status = Status.WAITING;
+        scale = Scale.FAHRENHEIT;
+        isConvertButtonEnable = false;
+    }
+
+    public String getInputValue() {
+        return inputValue;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setInputValue(final String inputValue) {
+        this.inputValue = inputValue;
+    }
+
+    public void setResult(final String result) {
+        this.result = result;
+    }
+
+    public Scale getScale() {
+        return scale;
+    }
+
+    public boolean isConvertButtonEnabled() {
+        return isConvertButtonEnable;
+    }
+
+    public void setScale(final Scale scale) {
+        this.scale = scale;
+    }
+
+    private boolean isInputValueNotEmpty() {
+        return !inputValue.isEmpty();
+    }
+
+    private boolean parseInput() {
+        try {
+            if (isInputValueNotEmpty()) {
+                Double.parseDouble(inputValue);
+            }
+        } catch (Exception e) {
+            status = Status.WRONG_FORMAT;
+            isConvertButtonEnable = false;
+            return false;
+        }
+
+       isConvertButtonEnable = isInputValueNotEmpty();
+
+        if (isConvertButtonEnable) {
+            status = Status.READY;
+        } else {
+            status = Status.WAITING;
+        }
+        return isConvertButtonEnable;
+    }
+
+    public void convert() {
+        if (!parseInput()) {
+           return;
+       }
+
+        TemperatureConverter converter = new TemperatureConverter();
+        double temperature;
+
+        switch (scale) {
+            case FAHRENHEIT:
+                temperature = converter.celsiusToFahrenheit(Double.parseDouble(inputValue));
+                break;
+            case KELVIN:
+                temperature = converter.celsiusToKelvin(Double.parseDouble(inputValue));
+                break;
+            case NEWTON:
+                temperature = converter.celsiusToNewton(Double.parseDouble(inputValue));
+                break;
+            default:
+                throw new IllegalArgumentException("You can use only Fahrenheit, Kelvin or Newton");
+        }
+
+        result = Double.toString(temperature);
+        status = Status.SUCCESS;
+    }
+
+    public final class Status {
+       public static final String WAITING = "Please, enter value";
+       public static final String READY = "Press 'Convert'";
+       public static final String WRONG_FORMAT = "Wrong input values";
+       public static final String SUCCESS = "Success";
+
+        private Status() { }
+    }
+
+    public enum Scale {
+        FAHRENHEIT("Fahrenheit"),
+        KELVIN("Kelvin"),
+        NEWTON("Newton");
+        private final String name;
+
+        private Scale(final String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return name;
+        }
+    }
+}
+
---
+++ code/blinkova-ekaterina/ViewModel/src/test/java/ru/unn/agile/TemperatureConverter/viewmodel/ViewModelTests.java
@@ -0,0 +1,128 @@
+package ru.unn.agile.TemperatureConverter.viewmodel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.unn.agile.TemperatureConverter.viewmodel.ViewModel.Status;
+import ru.unn.agile.TemperatureConverter.viewmodel.ViewModel.Scale;
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void canSetDefaultValues() {
+        assertEquals("", viewModel.getInputValue());
+        assertEquals("", viewModel.getResult());
+        assertEquals(Status.WAITING, viewModel.getStatus());
+        assertEquals(Scale.FAHRENHEIT, viewModel.getScale());
+        assertEquals(false, viewModel.isConvertButtonEnabled());
+    }
+
+    @Test
+    public void isStatusWaitingInTheBeginning() {
+        assertEquals(Status.WAITING, viewModel.getStatus());
+    }
+
+    @Test
+    public void canSetSomeInputValue() {
+        viewModel.setInputValue("1.0");
+        assertEquals("1.0", viewModel.getInputValue());
+    }
+
+    @Test
+    public void canSetResult() {
+        viewModel.setResult("1.0");
+        assertEquals("1.0", viewModel.getResult());
+    }
+
+    @Test
+    public void canGetScaleName() {
+        String fahrenheitName = Scale.FAHRENHEIT.toString();
+        assertEquals("Fahrenheit", fahrenheitName);
+    }
+
+    @Test
+    public void canSetFahrenheitScale() {
+        viewModel.setScale(Scale.FAHRENHEIT);
+        assertEquals(Scale.FAHRENHEIT, viewModel.getScale());
+    }
+
+    @Test
+    public void canSetKelvinScale() {
+        viewModel.setScale(Scale.KELVIN);
+        assertEquals(Scale.KELVIN, viewModel.getScale());
+    }
+
+    @Test
+    public void canSetNewtonScale() {
+        viewModel.setScale(Scale.NEWTON);
+        assertEquals(Scale.NEWTON, viewModel.getScale());
+    }
+
+    @Test
+    public void canCompareScalesByName() {
+        assertEquals(Scale.NEWTON, Scale.NEWTON);
+        assertNotEquals(Scale.NEWTON, Scale.FAHRENHEIT);
+    }
+
+    @Test
+    public void canCreateListOfScales() {
+        ViewModel.Scale[] scales = ViewModel.Scale.values();
+        ViewModel.Scale[] realScales = new Scale[]{
+                Scale.FAHRENHEIT,
+                Scale.KELVIN,
+                Scale.NEWTON};
+        assertArrayEquals(realScales, scales);
+    }
+
+    @Test
+    public void canConvertToFahrenheit() {
+        viewModel.setInputValue("1.0");
+        viewModel.setScale(Scale.FAHRENHEIT);
+        viewModel.convert();
+        assertEquals("33.8", viewModel.getResult());
+    }
+
+    @Test
+    public void canConvertToKelvin() {
+        viewModel.setInputValue("1.0");
+        viewModel.setScale(Scale.KELVIN);
+        viewModel.convert();
+        assertEquals("274.0", viewModel.getResult());
+    }
+
+    @Test
+    public void canConvertToNewton() {
+        viewModel.setInputValue("1.0");
+        viewModel.setScale(Scale.NEWTON);
+        viewModel.convert();
+        assertEquals("0.33", viewModel.getResult());
+    }
+
+    @Test
+    public void isStatusWrongFormatThenBadInputValue() {
+        viewModel.setInputValue("q");
+        viewModel.convert();
+        assertEquals(Status.WRONG_FORMAT, viewModel.getStatus());
+  }
+
+    @Test
+    public void isStatusSuccessThenGoodInputValue() {
+        viewModel.setInputValue("1.0");
+        viewModel.convert();
+        assertEquals(Status.SUCCESS, viewModel.getStatus());
+    }
+}

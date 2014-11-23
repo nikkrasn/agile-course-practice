6e59e5ec632074f8af79d81755033d388ab3338e
BlinkovaEkaterina <k-06-11@mail.ru>
2014-11-23 21:49:49 +0300

    Added fake test, made bind, backbind

--- code/blinkova-ekaterina/View/src/main/java/ru/unn/agile/TemperatureConverter/view/Converter.java
+++ code/blinkova-ekaterina/View/src/main/java/ru/unn/agile/TemperatureConverter/view/Converter.java
@@ -1,23 +1,82 @@
 package ru.unn.agile.TemperatureConverter.view;
 
 import javax.swing.*;
+import ru.unn.agile.TemperatureConverter.viewmodel.ViewModel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public final class Converter {
 
     private JPanel mainJPanel;
-    private JComboBox cbScale;
     private JTextField txtValue;
     private JTextField txtResult;
     private JButton btnConvert;
-    private JTextField txtErrorMessage;
+    private JTextField txtStatus;
+    private JComboBox<ViewModel.Scale> cbScale;
+    private ViewModel viewModel;
 
     private Converter() { }
 
+    private Converter(final ViewModel viewModel) {
+        this.viewModel = viewModel;
+        backBind();
+
+        loadListOfScales();
+
+        btnConvert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent actionEvent) {
+                bind();
+                Converter.this.viewModel.convert();
+                backBind();
+            }
+        });
+
+        cbScale.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent actionEvent) {
+                bind();
+                backBind();
+            }
+        });
+
+        KeyAdapter keyListener = new KeyAdapter() {
+            @Override
+            public void keyReleased(final KeyEvent keyEvent) {
+                bind();
+                Converter.this.viewModel.processKeyInTextField();
+                backBind();
+            }
+        };
+        txtValue.addKeyListener(keyListener);
+   }
+
+
+
     public static void main(final String[] args) {
         JFrame frame = new JFrame("Converter");
-        frame.setContentPane(new Converter().mainJPanel);
+        frame.setContentPane(new Converter(new ViewModel()).mainJPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void loadListOfScales() {
+        ViewModel.Scale[] scales = ViewModel.Scale.values();
+        cbScale.setModel(new JComboBox<>(scales).getModel());
+    }
+
+    private void bind() {
+        viewModel.setInputValue(txtValue.getText());
+        viewModel.setScale((ViewModel.Scale) cbScale.getSelectedItem());
+    }
+
+    private void backBind() {
+        btnConvert.setEnabled(viewModel.isConvertButtonEnabled());
+        txtResult.setText(viewModel.getResult());
+        txtStatus.setText(viewModel.getStatus());
     }
 }

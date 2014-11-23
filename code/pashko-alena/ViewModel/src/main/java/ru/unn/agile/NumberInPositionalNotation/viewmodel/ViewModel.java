178aa415e106f0b65bbfc19ae28421d624fb4108
alena-p- <alena.pashko@gmail.com>
2014-11-23 23:25:20 +0300

    Added ViewModel

---
+++ code/pashko-alena/ViewModel/src/main/java/ru/unn/agile/NumberInPositionalNotation/viewmodel/ViewModel.java
@@ -0,0 +1,160 @@
+package ru.unn.agile.NumberInPositionalNotation.viewmodel;
+
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import ru.unn.agile.NumberInPositionalNotation.Model.NumberInPositionalNotation;
+import ru.unn.agile.NumberInPositionalNotation.Model.Notation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewModel {
+    private final StringProperty inputNumber = new SimpleStringProperty();
+
+    private final ObjectProperty<ObservableList<Notation>> notations =
+            new SimpleObjectProperty<>(FXCollections.observableArrayList(Notation.values()));
+    private final ObjectProperty<Notation> inputNotation = new SimpleObjectProperty<>();
+    private final ObjectProperty<Notation> outputNotation = new SimpleObjectProperty<>();
+    private final BooleanProperty convertDisabled = new SimpleBooleanProperty();
+    private final StringProperty inputNotationStr = new SimpleStringProperty();
+
+    private final StringProperty status = new SimpleStringProperty();
+    private final StringProperty outputNumber = new SimpleStringProperty();
+
+    private final List<ValueChangeListener> valueChangedListeners = new ArrayList<>();
+
+    public ViewModel() {
+        inputNumber.set("");
+        inputNotation.set(Notation.BINARY);
+        outputNotation.set(Notation.DECIMAL);
+        inputNotationStr.set(inputNotation.toString());
+        outputNumber.set("");
+        status.set(Status.WAITING.toString());
+
+        BooleanBinding couldCalculate = new BooleanBinding() {
+            {
+                super.bind(inputNumber, inputNotationStr);
+            }
+            @Override
+            protected boolean computeValue() {
+                return getInputStatus() == Status.READY;
+            }
+        };
+        convertDisabled.bind(couldCalculate.not());
+
+        final List<StringProperty> fields = new ArrayList<StringProperty>() { {
+            add(inputNumber);
+            add(inputNotationStr);
+        } };
+
+        for (StringProperty field : fields) {
+            final ValueChangeListener listener = new ValueChangeListener();
+            field.addListener(listener);
+            valueChangedListeners.add(listener);
+        }
+    }
+
+    private Status getInputStatus() {
+        Status inputStatus;
+            if (inputNumber.get().isEmpty()) {
+                inputStatus = Status.WAITING;
+            } else {
+                NumberInPositionalNotation number =
+                        new NumberInPositionalNotation(inputNumber.get(), inputNotation.get());
+                if (number.checkInputData()) {
+                    inputStatus = Status.READY;
+                } else {
+                    inputStatus = Status.BAD_FORMAT;
+                }
+            }
+        return inputStatus;
+    }
+
+    public void convert() {
+        if (convertDisabled.get()) {
+            return;
+        }
+        NumberInPositionalNotation input =
+                new NumberInPositionalNotation(inputNumber.get(), inputNotation.get());
+        NumberInPositionalNotation output;
+        output = input.convertToNotation(outputNotation.get());
+
+        outputNumber.set(output.getValue());
+        status.set(Status.SUCCESS.toString());
+    }
+
+    public StringProperty inputNumberProperty() {
+        return inputNumber;
+    }
+
+    public ObjectProperty<Notation> inputNotationProperty() {
+        return inputNotation;
+    }
+
+    public ObjectProperty<Notation> outputNotationProperty() {
+        return outputNotation;
+    }
+
+    public StringProperty outputNumberProperty() {
+        return outputNumber;
+    }
+
+    public StringProperty statusProperty() {
+        return status;
+    }
+
+    public BooleanProperty convertDisabledProperty() {
+        return convertDisabled;
+    }
+
+    public ObjectProperty<ObservableList<Notation>> notationsProperty() {
+        return notations;
+    }
+
+    public final boolean getConvertDisabled() {
+        return convertDisabled.get();
+    }
+
+    public final String getOutputNumber() {
+        return outputNumber.get();
+    }
+
+    public final ObservableList<Notation> getNotations() {
+        inputNotationStr.set(inputNotation.toString());
+        return notations.get();
+    }
+
+    public final String getStatus() {
+        return status.get();
+    }
+
+    private class ValueChangeListener implements ChangeListener<String> {
+        @Override
+        public void changed(final ObservableValue<? extends String> observable,
+                            final String oldValue, final String newValue) {
+            status.set(getInputStatus().toString());
+        }
+    }
+}
+
+enum Status {
+    WAITING("Please provide input data"),
+    READY("Press 'Convert' or Enter"),
+    BAD_FORMAT("Bad format"),
+    SUCCESS("Success");
+
+    private final String name;
+
+    private Status(final String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
---
+++ code/pashko-alena/ViewModel/src/test/java/ru/unn/agile/NumberInPositionalNotation/viewmodel/ViewModelTests.java
@@ -0,0 +1,162 @@
+package ru.unn.agile.NumberInPositionalNotation.viewmodel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.unn.agile.NumberInPositionalNotation.Model.Notation;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void canSetDefaultValues() {
+        assertEquals("", viewModel.inputNumberProperty().get());
+        assertEquals(Notation.BINARY, viewModel.inputNotationProperty().get());
+        assertEquals(Notation.DECIMAL, viewModel.outputNotationProperty().get());
+        assertEquals("", viewModel.outputNumberProperty().get());
+        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusIsWaitingWhenConvertWithEmptyFields() {
+        viewModel.convert();
+
+        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    }
+
+    private void setInputData() {
+        viewModel.inputNumberProperty().set("1");
+        viewModel.inputNotationProperty().set(Notation.BINARY);
+        viewModel.outputNotationProperty().set(Notation.DECIMAL);
+    }
+
+    @Test
+    public void statusIsReadyWhenFieldsAreFill() {
+        setInputData();
+
+        assertEquals(Status.READY.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void canReportBadFormat() {
+        viewModel.inputNumberProperty().set("f");
+
+        assertEquals(Status.BAD_FORMAT.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusIsWaitingIfNotEnoughCorrectData() {
+        viewModel.inputNotationProperty().set(Notation.OCTAL);
+        viewModel.outputNotationProperty().set(Notation.BINARY);
+
+        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void convertButtonIsDisabledInitially() {
+        assertTrue(viewModel.convertDisabledProperty().get());
+    }
+
+    @Test
+    public void convertButtonIsDisabledWhenFormatIsBad() {
+        setInputData();
+        viewModel.inputNumberProperty().set("12y");
+
+        assertTrue(viewModel.convertDisabledProperty().get());
+    }
+
+    @Test
+    public void convertButtonIsDisabledWithIncompleteInput() {
+        viewModel.inputNotationProperty().set(Notation.HEXADECIMAL);
+        viewModel.outputNotationProperty().set(Notation.OCTAL);
+
+        assertTrue(viewModel.convertDisabledProperty().get());
+    }
+
+    @Test
+    public void convertButtonIsEnabledWithCorrectInput() {
+        setInputData();
+
+        assertFalse(viewModel.convertDisabledProperty().get());
+    }
+
+    @Test
+    public void canSetInputNotation() {
+        viewModel.inputNotationProperty().set(Notation.OCTAL);
+        assertEquals(Notation.OCTAL, viewModel.inputNotationProperty().get());
+    }
+
+    @Test
+    public void canSetOutputNotation() {
+        viewModel.outputNotationProperty().set(Notation.DECIMAL);
+        assertEquals(Notation.DECIMAL, viewModel.outputNotationProperty().get());
+    }
+
+    @Test
+    public void binaryIsDefaultInputNotation() {
+        assertEquals(Notation.BINARY, viewModel.inputNotationProperty().get());
+    }
+
+    @Test
+    public void decimalIsDefaultOutputNotation() {
+        assertEquals(Notation.DECIMAL, viewModel.outputNotationProperty().get());
+    }
+
+    @Test
+    public void covertHasCorrectResult() {
+        viewModel.inputNumberProperty().set("1.1");
+        viewModel.inputNotationProperty().set(Notation.BINARY);
+        viewModel.outputNotationProperty().set(Notation.DECIMAL);
+
+        viewModel.convert();
+
+        assertEquals("1.5", viewModel.outputNumberProperty().get());
+    }
+
+    @Test
+    public void canSetSuccessMessage() {
+        setInputData();
+
+        viewModel.convert();
+
+        assertEquals(Status.SUCCESS.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void canSetBadFormatMessage() {
+        viewModel.inputNumberProperty().set("smthng");
+
+        assertEquals(Status.BAD_FORMAT.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusIsReadyWhenSetProperData() {
+        setInputData();
+
+        assertEquals(Status.READY.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void convertToHexadecimalHasCorrectResult() {
+        viewModel.inputNumberProperty().set("10.7");
+        viewModel.inputNotationProperty().set(Notation.OCTAL);
+        viewModel.outputNotationProperty().set(Notation.HEXADECIMAL);
+
+        viewModel.convert();
+
+        assertEquals("8.e", viewModel.outputNumberProperty().get());
+    }
+}

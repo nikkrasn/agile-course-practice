ba64a226a7535d5445e799b7cee8b30efdc876b6
Bobylkov Fedor <bobylkov.f@gmail.com>
2014-11-23 18:51:40 +0300

    form created

--- code/bobylkov-fedor/View/src/main/java/ru/unn/agile/Stack/View/Main.java
+++ code/bobylkov-fedor/View/src/main/java/ru/unn/agile/Stack/View/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(final Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Stack.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Stack");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }

1f1c3c8ea0f2f3d45c9622ea2991ffe2e28832a1
Evgenii Izutov <izutov.e.o@gmail.com>
2014-11-24 16:33:27 +0300

    Added binding to View.

--- code/izutov-evgenii/View/src/main/java/ru/unn/agile/DemandElasticity/view/DemandElasticity.java
+++ code/izutov-evgenii/View/src/main/java/ru/unn/agile/DemandElasticity/view/DemandElasticity.java
@@ -6,8 +6,41 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-//import ru.unn.agile.DemandElasticity.viewmodel.ViewModel;
+import ru.unn.agile.DemandElasticity.viewmodel.DemandElasticityType;
+import ru.unn.agile.DemandElasticity.viewmodel.ViewModel;
 
 public class DemandElasticity {
+    @FXML
+    private ViewModel viewModel;
+    @FXML
+    private TextField txtRange1Start;
+    @FXML
+    private TextField txtRange1Finish;
+    @FXML
+    private TextField txtRange2Start;
+    @FXML
+    private TextField txtRange2Finish;
+    @FXML
+    private ComboBox<DemandElasticityType> cbDemandElasticityType;
+    @FXML
+    private Button btnCalculate;
 
+    @FXML
+    void initialize() {
+        // Two-way binding hasn't supported by FXML yet, so place it in code-behind
+        txtRange1Start.textProperty().bindBidirectional(viewModel.start1Property());
+        txtRange1Finish.textProperty().bindBidirectional(viewModel.finish1Property());
+        txtRange2Start.textProperty().bindBidirectional(viewModel.start2Property());
+        txtRange2Finish.textProperty().bindBidirectional(viewModel.finish2Property());
+
+        cbDemandElasticityType.valueProperty().bindBidirectional(
+                viewModel.demandElasticityTypeProperty());
+
+        btnCalculate.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent event) {
+                viewModel.calculate();
+            }
+        });
+    }
 }

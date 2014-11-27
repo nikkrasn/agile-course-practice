df6b660ab6277ea1f7439b48bd21acc6e6a489d8
SerovAlexey <SerovAlexey1993@gmail.com>
2014-11-28 01:16:41 +0300

    test

--- code/Serov-Alexey/View/src/main/java/ru/unn/agile/ConverterWeight/view/Converter.java
+++ code/Serov-Alexey/View/src/main/java/ru/unn/agile/ConverterWeight/view/Converter.java
@@ -18,6 +18,16 @@ public final class Converter {
     private JComboBox<UnitWeight> cbSourceUnit;
     private JComboBox<UnitWeight> cbEndUnit;
 
+    public static void main(final String[] args) {
+        JFrame frame = new JFrame("Converter");
+        frame.setContentPane(new Converter(new ViewModel()).mainJPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private Converter() { }
+
     private Converter(final ViewModel viewModel) {
         this.viewModel = viewModel;
         backBind();
@@ -59,16 +69,6 @@ public final class Converter {
 
         txtValues.addKeyListener(keyListener);
     }
-
-    public static void main(final String[] args) {
-        JFrame frame = new JFrame("Converter");
-        frame.setContentPane(new Converter(new ViewModel()).mainJPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    private Converter() { }
 
     private void loadListOfUnitWeight() {
         UnitWeight[] unitWeights = UnitWeight.values();

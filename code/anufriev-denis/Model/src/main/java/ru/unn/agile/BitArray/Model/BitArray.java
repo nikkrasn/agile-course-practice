c32cfc6da4786457c11872ed506b9bed15879c3c
AnufrievDE <anufriev.denis.e@gmail.com>
2014-11-27 20:57:17 +0300

    unnecessary using of this deleted

--- code/anufriev-denis/Model/src/main/java/ru/unn/agile/BitArray/Model/BitArray.java
+++ code/anufriev-denis/Model/src/main/java/ru/unn/agile/BitArray/Model/BitArray.java
@@ -16,7 +16,7 @@ public class BitArray {
     }
 
     public int[] getArray() {
-        return this.array;
+        return array;
     }
 
     public BitArray setBit(final int index) {
@@ -25,7 +25,7 @@ public class BitArray {
         int position = index % BIT_PER_INT;
 
         int bitMask = BIT_MASK << position;
-        this.array[intArrayIndex] = this.array[intArrayIndex] | bitMask;
+        array[intArrayIndex] = array[intArrayIndex] | bitMask;
         return this;
     }
 
@@ -37,7 +37,7 @@ public class BitArray {
         int bitMask = BIT_MASK << position;
         bitMask = ~bitMask;
 
-        this.array[intArrayIndex] = this.array[intArrayIndex] & bitMask;
+        array[intArrayIndex] = array[intArrayIndex] & bitMask;
         return this;
     }
 
@@ -61,42 +61,42 @@ public class BitArray {
 
         int bitMask = BIT_MASK << position;
 
-        return (this.array[intArrayIndex] & bitMask) != 0;
+        return (array[intArrayIndex] & bitMask) != 0;
     }
 
-    public BitArray xor(final BitArray array) {
+    public BitArray xor(final BitArray a) {
         for (int i = 0; i < (size + BIT_PER_INT - 1) / BIT_PER_INT; i++) {
-            this.array[i] = this.array[i] ^ array.getArray()[i];
+            array[i] = array[i] ^ a.getArray()[i];
         }
         return this;
     }
 
-    public BitArray or(final BitArray array) {
+    public BitArray or(final BitArray a) {
         for (int i = 0; i < (size + BIT_PER_INT - 1) / BIT_PER_INT; i++) {
-            this.array[i] = this.array[i] | array.getArray()[i];
+            array[i] = array[i] | a.getArray()[i];
         }
         return this;
     }
 
-    public BitArray and(final BitArray array) {
+    public BitArray and(final BitArray a) {
         for (int i = 0; i < (size + BIT_PER_INT - 1) / BIT_PER_INT; i++) {
-            this.array[i] = this.array[i] & array.getArray()[i];
+            array[i] = array[i] & a.getArray()[i];
         }
         return this;
     }
 
     public BitArray not() {
-        for (int i = 0; i < (this.size + BIT_PER_INT - 1) / BIT_PER_INT; i++) {
-            this.array[i] = ~this.array[i];
+        for (int i = 0; i < (size + BIT_PER_INT - 1) / BIT_PER_INT; i++) {
+            array[i] = ~array[i];
         }
-        int bitMask = getBitMask(this.size);
-        this.array[(size + BIT_PER_INT - 1) / BIT_PER_INT - 1] &= bitMask;
+        int bitMask = getBitMask(size);
+        array[(size + BIT_PER_INT - 1) / BIT_PER_INT - 1] &= bitMask;
         return this;
     }
 
     public String toString() {
         String result = "";
-        for (int i = 0; i < this.size; i++) {
+        for (int i = 0; i < size; i++) {
             if (this.get(i)) {
                 result += "1";
             } else {
@@ -137,7 +137,7 @@ public class BitArray {
     }
 
     private void checkOutOfArray(final int index) {
-        if (index >= this.size || index < 0) {
+        if (index >= size || index < 0) {
             throw new IllegalArgumentException("Out of array");
         }
     }

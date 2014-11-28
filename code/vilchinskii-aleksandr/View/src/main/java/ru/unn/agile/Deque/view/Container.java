a5086a27454af1ddc057c0e5e5cb173debdfcb47
Aleksandr Vilchinskii <vilchinsky91@mail.ru>
2014-11-28 04:33:25 +0400

    deque-view: initial commit

---
+++ code/vilchinskii-aleksandr/View/src/main/java/ru/unn/agile/Deque/view/Container.java
@@ -0,0 +1,26 @@
+package ru.unn.agile.Deque.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Container {
+    @FXML
+    private TextField txtValue;
+    @FXML
+    private Button btnAddFirst;
+    @FXML
+    private Button btnAddLast;
+    @FXML
+    private Button btnGetFirst;
+    @FXML
+    private Button btnGetLast;
+    @FXML
+    private Button btnRemoveFirst;
+    @FXML
+    private Button btnRemoveLast;
+
+    @FXML
+    void initialize() {
+    }
+}

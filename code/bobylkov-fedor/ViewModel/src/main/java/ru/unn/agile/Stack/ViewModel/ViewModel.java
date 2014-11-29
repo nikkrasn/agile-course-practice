1a12178153c2face05eb62523b474524346236a4
Bobylkov Fedor <bobylkov.f@gmail.com>
2014-11-29 22:02:55 +0300

    first viewmodel test was added

--- code/bobylkov-fedor/ViewModel/src/main/java/ru/unn/agile/Stack/ViewModel/ViewModel.java
+++ code/bobylkov-fedor/ViewModel/src/main/java/ru/unn/agile/Stack/ViewModel/ViewModel.java
@@ -1,4 +1,20 @@
 package ru.unn.agile.Stack.ViewModel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ViewModel {
+    private final StringProperty top = new SimpleStringProperty();
+    private final StringProperty push = new SimpleStringProperty();
+    public StringProperty top() {
+        return top;
+    }
+    public StringProperty push() {
+        return push;
+    }
+
+    public ViewModel() {
+        top.set("");
+        push.set("Push me!");
+    }
 }
--- code/bobylkov-fedor/ViewModel/src/test/java/ru/unn/agile/Stack/ViewModel/ViewModelTests.java
+++ code/bobylkov-fedor/ViewModel/src/test/java/ru/unn/agile/Stack/ViewModel/ViewModelTests.java
@@ -1,4 +1,21 @@
 package ru.unn.agile.Stack.ViewModel;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @Test
+    public void checkDefaultValues() {
+        assertEquals("", viewModel.top().get());
+        assertEquals("Push me!", viewModel.push().get());
+    }
 }

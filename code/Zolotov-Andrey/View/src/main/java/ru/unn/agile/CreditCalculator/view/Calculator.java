c632f10c1710d7f4df1a106ac3ac8e46cd43a2d2
AndreyZolotov <andreyzltv1993@gmail.com>
2014-12-01 00:41:43 +0300

    mistake

--- code/Zolotov-Andrey/View/src/main/java/ru/unn/agile/CreditCalculator/view/Calculator.java
+++ code/Zolotov-Andrey/View/src/main/java/ru/unn/agile/CreditCalculator/view/Calculator.java
@@ -5,8 +5,6 @@ import ru.unn.agile.CreditCalculator.ViewModel.ViewModel;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public final class Calculator {
 

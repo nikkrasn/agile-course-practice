5e11e89379aa19e91876b6b48b0bf044cfcdd9e1
Syainova.Diana@gmail.com <syainova.diana@gmail.com>
2014-11-30 23:52:12 +0400

    Formated ViewModelTests

--- code/Syainova-Diana/ViewModel/src/test/java/ru/unn/agile/QuadraticEquation/viewmodel/ViewModelTests.java
+++ code/Syainova-Diana/ViewModel/src/test/java/ru/unn/agile/QuadraticEquation/viewmodel/ViewModelTests.java
@@ -31,30 +31,35 @@ public class ViewModelTests {
     @Test
     public void statusIsWaitingWhenSolveWithEmptyFields() {
         viewModel.solve();
+
         assertEquals(systemStatus.WAITING.toString(), viewModel.statusProperty().get());
     }
 
     @Test
     public void statusIsReadyWhenFieldsAreFill() {
         setEquationData();
+
         assertEquals(systemStatus.READY.toString(), viewModel.statusProperty().get());
     }
 
     @Test
     public void statusIsIncorrectWhenFirstCoefficientIsNull() {
         viewModel.firstCoefficientProperty().set("0");
+
         assertEquals(systemStatus.INCORRECT_COEF.toString(), viewModel.statusProperty().get());
     }
 
     @Test
     public void solveIsIncorrectWhenFirstCoefficientIsNullOtherKind() {
         viewModel.firstCoefficientProperty().set("00.0");
+
         assertEquals(systemStatus.INCORRECT_COEF.toString(), viewModel.statusProperty().get());
     }
 
     @Test
     public void canReportWhenTheInputCoefficientHasBadFormat() {
         viewModel.firstCoefficientProperty().set("/");
+
         assertEquals(systemStatus.BAD_FORMAT.toString(), viewModel.statusProperty().get());
     }
 
@@ -62,6 +67,7 @@ public class ViewModelTests {
     public void statusIsWaitingWhenNotAllCoefficientsIntroduced() {
         viewModel.firstCoefficientProperty().set("2");
         viewModel.secondCoefficientProperty().set("1");
+
         assertEquals(systemStatus.WAITING.toString(), viewModel.statusProperty().get());
     }
 
@@ -74,6 +80,7 @@ public class ViewModelTests {
     public void solveButtonIsDisabledWhenFormatIsBad() {
         setEquationData();
         viewModel.freeTermProperty().set("ttt");
+
         assertTrue(viewModel.solvingDisabledProperty().get());
     }
 
@@ -81,6 +88,7 @@ public class ViewModelTests {
     public void solveButtonIsDisabledWhenFirstCoefficientIsNull() {
         setEquationData();
         viewModel.firstCoefficientProperty().set("0");
+
         assertTrue(viewModel.solvingDisabledProperty().get());
     }
 
@@ -88,12 +96,14 @@ public class ViewModelTests {
     public void solveButtonIsDisabledWheNotAllCoefficientsIntroduced() {
         viewModel.firstCoefficientProperty().set("4");
         viewModel.freeTermProperty().set("2");
+
         assertTrue(viewModel.solvingDisabledProperty().get());
     }
 
     @Test
     public void solveButtonIsEnabledWhenAllCorrectCoefficientIntroduced() {
         setEquationData();
+
         assertFalse(viewModel.solvingDisabledProperty().get());
     }
 
@@ -102,7 +112,9 @@ public class ViewModelTests {
         viewModel.firstCoefficientProperty().set("1");
         viewModel.secondCoefficientProperty().set("1");
         viewModel.freeTermProperty().set("-2");
+
         viewModel.solve();
+
         assertEquals("x = 1.0", viewModel.firstRootResultProperty().get());
         assertEquals("x = -2.0", viewModel.secondRootResultProperty().get());
     }
@@ -110,19 +122,23 @@ public class ViewModelTests {
     @Test
     public void statusIsSuccessWhenEquationCorrectlySolved() {
         setEquationData();
+
         viewModel.solve();
+
         assertEquals(systemStatus.SUCCESS.toString(), viewModel.statusProperty().get());
     }
 
     @Test
     public void statusIsReadyWhenEnterCorrectCoefficients() {
         setEquationData();
+
         assertEquals(systemStatus.READY.toString(), viewModel.statusProperty().get());
     }
 
     @Test
     public void canReportBadFormatCoefficients() {
         viewModel.firstCoefficientProperty().set("incorrect coefficients");
+
         assertEquals(systemStatus.BAD_FORMAT.toString(), viewModel.statusProperty().get());
     }
 
@@ -131,7 +147,9 @@ public class ViewModelTests {
         viewModel.firstCoefficientProperty().set("1");
         viewModel.secondCoefficientProperty().set("-10");
         viewModel.freeTermProperty().set("25");
+
         viewModel.solve();
+
         assertEquals("x = 5.0", viewModel.firstRootResultProperty().get());
         assertEquals("x = 5.0", viewModel.secondRootResultProperty().get());
     }
@@ -141,7 +159,9 @@ public class ViewModelTests {
         viewModel.firstCoefficientProperty().set("1");
         viewModel.secondCoefficientProperty().set("1");
         viewModel.freeTermProperty().set("2");
+
         viewModel.solve();
+
         assertEquals(systemStatus.NO_ROOTS.toString(), viewModel.statusProperty().get());
     }
 

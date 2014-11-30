aa2ea1c4f017d68654a259ded4dce1f50d7ac576
Ljaljushkin Nikolay <ljaljushkin.nikolaj@gmail.com>
2014-11-30 17:55:28 +0300

    Checked the status at filling fields.
    Code was changed according to the law of Demeter within reasonable limits.

--- code/Ljaljushkin-Nikolay/ViewModel/src/main/java/ru/unn/agile/ColorConverter/viewmodel/ViewModel.java
+++ code/Ljaljushkin-Nikolay/ViewModel/src/main/java/ru/unn/agile/ColorConverter/viewmodel/ViewModel.java
@@ -4,9 +4,14 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import ru.unn.agile.ColorConverter.model.ColorSpaces.ColorSpace;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewModel {
 
@@ -28,9 +33,9 @@ public class ViewModel {
 
     private final ObjectProperty<Color> dstColor = new SimpleObjectProperty<>();
 
-    //    private final BooleanProperty convertionDisabled = new SimpleBooleanProperty();
     private final StringProperty status = new SimpleStringProperty();
-    //    private final List<ValueChangeListener> valueChangedListeners = new ArrayList<>();
+
+    private final List<ValueChangeListener> valueChangedListeners = new ArrayList<>();
 
     // FXML needs default c-tor for binding
     public ViewModel() {
@@ -44,7 +49,84 @@ public class ViewModel {
 
         status.set(Status.WAITING.toString());
         srcColor.set(Color.RGB);
-        dstColor.set(Color.RGB);
+        dstColor.set(Color.LAB);
+
+        final List<StringProperty> fields = new ArrayList<StringProperty>() {
+            {
+                add(firstChannelSrcColor);
+                add(secondChannelSrcColor);
+                add(thirdChannelSrcColor);
+                add(firstChannelDstColor);
+                add(secondChannelDstColor);
+                add(thirdChannelDstColor);
+            }
+        };
+
+        for (StringProperty field : fields) {
+            final ValueChangeListener listener = new ValueChangeListener();
+            field.addListener(listener);
+            valueChangedListeners.add(listener);
+        }
+    }
+
+    public void convert() {
+        String temp = getStatus();
+    }
+
+    public void setFirstChannelSrcColor(final String value) {
+        firstChannelSrcColor.set(value);
+    }
+
+    public void setSecondChannelSrcColor(final String value) {
+        secondChannelSrcColor.set(value);
+    }
+
+    public void setThirdChannelSrcColor(final String value) {
+        thirdChannelSrcColor.set(value);
+    }
+
+    public void setFirstChannelDstColor(final String value) {
+        firstChannelDstColor.set(value);
+    }
+
+    public void setSecondChannelDstColor(final String value) {
+        secondChannelDstColor.set(value);
+    }
+
+    public void setThirdChannelDstColor(final String value) {
+        thirdChannelDstColor.set(value);
+    }
+
+    public void setStatus(final String value) {
+        status.set(value);
+    }
+
+    public String getFirstChannelSrcColor() {
+        return firstChannelSrcColor.get();
+    }
+
+    public String getSecondChannelSrcColor() {
+        return secondChannelSrcColor.get();
+    }
+
+    public String getThirdChannelSrcColor() {
+        return thirdChannelSrcColor.get();
+    }
+
+    public String getFirstChannelDstColor() {
+        return firstChannelDstColor.get();
+    }
+
+    public String getSecondChannelDstColor() {
+        return secondChannelDstColor.get();
+    }
+
+    public String getThirdChannelDstColor() {
+        return thirdChannelDstColor.get();
+    }
+
+    public String getStatus() {
+        return status.get();
     }
 
     public StringProperty firstChannelSrcColorProperty() {
@@ -71,24 +153,60 @@ public class ViewModel {
         return thirdChannelDstColor;
     }
 
-    public ObjectProperty<ObservableList<Color>> srcColorsProperty() {
+    public ObjectProperty<ObservableList<Color>> getSrcColorsProperty() {
         return srcColors;
     }
 
-    public ObjectProperty<Color> srcColorProperty() {
-        return srcColor;
+    public Color getSrcColorValue() {
+        return srcColor.get();
     }
 
-    public ObjectProperty<ObservableList<Color>> dstColorsProperty() {
+    public ObjectProperty<ObservableList<Color>> getDstColorsProperty() {
         return dstColors;
     }
 
-    public ObjectProperty<Color> dstColorProperty() {
-        return dstColor;
+    public Color getDstColorValue() {
+        return dstColor.get();
     }
 
     public StringProperty statusProperty() {
         return status;
+    }
+
+    public String getSrcColor() {
+        return getSrcColorValue().toString();
+    }
+
+    public String getDstColor() {
+        return getDstColorValue().toString();
+    }
+
+    private Status getInputStatus() {
+        Status result = Status.READY;
+        if (isSrcColorEmpty() || isDstColorEmpty()) {
+            result = Status.WAITING;
+        }
+        return result;
+    }
+
+    private boolean isSrcColorEmpty() {
+        return getFirstChannelSrcColor().isEmpty()
+                || getSecondChannelSrcColor().isEmpty()
+                || getThirdChannelSrcColor().isEmpty();
+    }
+
+    private boolean isDstColorEmpty() {
+        return getFirstChannelDstColor().isEmpty()
+                || getSecondChannelDstColor().isEmpty()
+                || getThirdChannelDstColor().isEmpty();
+    }
+
+    private class ValueChangeListener implements ChangeListener<String> {
+        @Override
+        public void changed(final ObservableValue<? extends String> observable,
+                            final String oldValue, final String newValue) {
+            status.set(getInputStatus().toString());
+        }
     }
 }
 
--- code/Ljaljushkin-Nikolay/ViewModel/src/test/java/ru/unn/agile/ColorConverter/viewmodel/ViewModelTests.java
+++ code/Ljaljushkin-Nikolay/ViewModel/src/test/java/ru/unn/agile/ColorConverter/viewmodel/ViewModelTests.java
@@ -1,5 +1,7 @@
 package ru.unn.agile.ColorConverter.viewmodel;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,20 +9,59 @@ import static org.junit.Assert.assertEquals;
 public class ViewModelTests {
     private ViewModel viewModel;
 
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
     @Test
     public void canSetDefaultValues() {
-        viewModel = new ViewModel();
+        assertEquals("", viewModel.getFirstChannelSrcColor());
+        assertEquals("", viewModel.getSecondChannelSrcColor());
+        assertEquals("", viewModel.getThirdChannelSrcColor());
 
-        assertEquals("", viewModel.firstChannelSrcColorProperty().get());
-        assertEquals("", viewModel.secondChannelSrcColorProperty().get());
-        assertEquals("", viewModel.thirdChannelSrcColorProperty().get());
+        assertEquals("", viewModel.getFirstChannelDstColor());
+        assertEquals("", viewModel.getSecondChannelDstColor());
+        assertEquals("", viewModel.getThirdChannelDstColor());
 
-        assertEquals("", viewModel.firstChannelDstColorProperty().get());
-        assertEquals("", viewModel.secondChannelDstColorProperty().get());
-        assertEquals("", viewModel.thirdChannelDstColorProperty().get());
+        assertEquals(Color.RGB.toString(), viewModel.getSrcColor());
+        assertEquals(Color.LAB.toString(), viewModel.getDstColor());
+        assertEquals(Status.WAITING.toString(), viewModel.getStatus());
+    }
 
-        assertEquals(Color.RGB, viewModel.srcColorProperty().get());
-        assertEquals(Color.RGB, viewModel.dstColorProperty().get());
+    @Test
+    public void isStatusWaitingWhenCalculateWithEmptyFields() {
+        viewModel.convert();
+        assertEquals(Status.WAITING.toString(), viewModel.getStatus());
+    }
+
+    @Test
+    public void isStatusReadyWhenFieldsAreFill() {
+        fillInputFields();
+        assertEquals(Status.READY.toString(), viewModel.getStatus());
+    }
+
+    @Test
+    public void statusIsWaitingWhenNotAllFieldsAreFill() {
+        fillNotAllFields();
         assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    }
+
+    private void fillInputFields() {
+        viewModel.setFirstChannelSrcColor("0");
+        viewModel.setSecondChannelSrcColor("0");
+        viewModel.setThirdChannelSrcColor("0");
+        viewModel.setFirstChannelDstColor("0");
+        viewModel.setSecondChannelDstColor("0");
+        viewModel.setThirdChannelDstColor("0");
+    }
+
+    private void fillNotAllFields() {
+        viewModel.setFirstChannelSrcColor("0");
     }
 }

d5111cc7e1abffe70b125dea849e493439421822
AndreyZolotov <andreyzltv1993@gmail.com>
2014-11-30 20:02:55 +0300

    Fixed mistakes

--- code/Zolotov-Andrey/View/src/main/java/ru/unn/agile/CreditCalculator/view/Calculator.java
+++ code/Zolotov-Andrey/View/src/main/java/ru/unn/agile/CreditCalculator/view/Calculator.java
@@ -2,11 +2,11 @@ package ru.unn.agile.CreditCalculator.view;
 
 //import ru.unn.agile.ComplexNumber.viewmodel.legacy.ViewModel;
 
-import javax.swing.*;
+/*import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.KeyEvent;*/
 
 public final class Calculator {
 /*

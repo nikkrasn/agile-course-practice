7ea52e10928eb61435aca1d93a78f2813169cca5
andrey-ovsuhno <kselar@gmail.com>
2014-12-01 20:24:22 +0400

    one more blank line

--- code/ovsuhno-andrey/ViewModel/src/test/java/ru/unn/agile/Triangle/viewmodel/ViewModelTest.java
+++ code/ovsuhno-andrey/ViewModel/src/test/java/ru/unn/agile/Triangle/viewmodel/ViewModelTest.java
@@ -36,6 +36,7 @@ public class ViewModelTest {
     @Test
     public void statusIsWaitingWhenComputeWithEmptyFields() {
         viewModel.compute();
+        
         assertEquals(CurrentStatus.WAITING.toString(), viewModel.statusProperty().get());
     }
 

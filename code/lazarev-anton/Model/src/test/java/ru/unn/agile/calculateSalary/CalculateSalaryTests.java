fdec516f4bb4bfd3fe9ecd453de583f36ebd2766
Anton Lazarev <alone2904@gmail.com>
2014-12-02 10:07:37 +0300

    fix calculateSalaryTests

--- code/lazarev-anton/Model/src/test/java/ru/unn/agile/calculateSalary/CalculateSalaryTests.java
+++ code/lazarev-anton/Model/src/test/java/ru/unn/agile/calculateSalary/CalculateSalaryTests.java
@@ -6,6 +6,7 @@ import java.time.Month;
 import static org.junit.Assert.*;
 
 public class CalculateSalaryTests {
+    private final double delta = 1e-10;
 
     @Test
     public void canCalculateCashWithNegativeSalary() {
@@ -40,7 +41,7 @@ public class CalculateSalaryTests {
                 .setSalary(10000)
                 .setCountingMonth(LocalDate.of(2014, Month.OCTOBER, 1))
                 .setWorkedHourInMonth(184);
-        assertEquals(8700.0, calculator.calculate(), 0.1);
+        assertEquals(8700.0, calculator.calculate(), delta);
     }
 
     @Test
@@ -49,7 +50,7 @@ public class CalculateSalaryTests {
                 .setSalary(10000)
                 .setCountingMonth(LocalDate.of(2014, Month.OCTOBER, 1))
                 .setWorkedHourInMonth(200);
-        assertEquals(10213.0, calculator.calculate(), 0.1);
+        assertEquals(10213.04347826086548, calculator.calculate(), delta);
     }
 
     @Test
@@ -58,7 +59,7 @@ public class CalculateSalaryTests {
                 .setSalary(10000)
                 .setCountingMonth(LocalDate.of(2014, Month.OCTOBER, 1))
                 .setWorkedHourInMonth(154);
-        assertEquals(7281.0, calculator.calculate(), 1.0);
+        assertEquals(7281.52173913043187, calculator.calculate(), delta);
     }
 
     @Test
@@ -69,7 +70,7 @@ public class CalculateSalaryTests {
                 .setWorkedHourInMonth(104)
                 .setStartOfVacation(LocalDate.of(2014, Month.OCTOBER, 6))
                 .setLengthOfVacation(14);
-        assertEquals(4917.0, calculator.calculate(), 1.0);
+        assertEquals(4917.39130434782412, calculator.calculate(), delta);
     }
 
     @Test
@@ -77,32 +78,21 @@ public class CalculateSalaryTests {
         CalculateSalary calculator = new CalculateSalary()
                 .setSalary(10000)
                 .setCountingMonth(LocalDate.of(2014, Month.OCTOBER, 1))
-                .setWorkedHourInMonth(104)
+                .setWorkedHourInMonth(144)
                 .setStartOfVacation(LocalDate.of(2014, Month.OCTOBER, 27))
                 .setLengthOfVacation(14);
-        assertEquals(4917.0, calculator.calculate(), 1.0);
+        assertEquals(6808.69565217391032, calculator.calculate(), delta);
     }
 
     @Test
     public void countCashInMonthWithEndOfVacation() {
         CalculateSalary calculator = new CalculateSalary()
                 .setSalary(10000)
-                .setCountingMonth(LocalDate.of(2014, Month.OCTOBER, 1))
-                .setWorkedHourInMonth(144)
-                .setStartOfVacation(LocalDate.of(2014, Month.OCTOBER, 27))
-                .setLengthOfVacation(14);
-        assertEquals(6808.0, calculator.calculate(), 1.0);
-    }
-
-    @Test
-    public void countCashInMonthWithEndOfVacationFromAnotherMonth() {
-        CalculateSalary calculator = new CalculateSalary()
-                .setSalary(10000)
                 .setCountingMonth(LocalDate.of(2014, Month.NOVEMBER, 1))
                 .setWorkedHourInMonth(120)
                 .setStartOfVacation(LocalDate.of(2014, Month.OCTOBER, 27))
                 .setLengthOfVacation(14);
-        assertEquals(6525.0, calculator.calculate(), 1.0);
+        assertEquals(6525.0, calculator.calculate(), delta);
     }
 
     @Test
@@ -113,7 +103,7 @@ public class CalculateSalaryTests {
                 .setWorkedHourInMonth(160)
                 .setStartOfVacation(LocalDate.of(2015, Month.OCTOBER, 27))
                 .setLengthOfVacation(14);
-        assertEquals(8700.0, calculator.calculate(), 1.0);
+        assertEquals(8700.0, calculator.calculate(), delta);
     }
 
     @Test
@@ -122,7 +112,7 @@ public class CalculateSalaryTests {
                 .setSalary(10000)
                 .setCountingMonth(LocalDate.of(2014, Month.NOVEMBER, 1))
                 .setWorkedHourInMonth(160);
-        assertEquals(8700.0, calculator.calculate(), 1.0);
+        assertEquals(8700.0, calculator.calculate(), delta);
     }
 
     @Test
@@ -133,6 +123,6 @@ public class CalculateSalaryTests {
                 .setWorkedHourInMonth(0)
                 .setStartOfVacation(LocalDate.of(2015, Month.SEPTEMBER, 27))
                 .setLengthOfVacation(50);
-        assertEquals(0.0, calculator.calculate(), 1.0);
+        assertEquals(0.0, calculator.calculate(), delta);
     }
 }

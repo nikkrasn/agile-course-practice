ab39e6fe5099bcf71ea68633f1d3a0ac16fe4a08
bobylkov-f <bobylkov.f@gmail.com>
2014-12-05 00:13:20 +0300

    Update ViewModel.java

--- code/bobylkov-fedor/ViewModel/src/main/java/ru/unn/agile/Stack/ViewModel/ViewModel.java
+++ code/bobylkov-fedor/ViewModel/src/main/java/ru/unn/agile/Stack/ViewModel/ViewModel.java
@@ -12,26 +12,39 @@ public class ViewModel {
     private final StringProperty pushText = new SimpleStringProperty();
     private final BooleanProperty isEmpty = new SimpleBooleanProperty();
     private final Stack<String> stack = new Stack<>();
+
     public StringProperty topProperty() {
         return top;
     }
     public final String getTop() {
         return top.get();
     }
+
     public StringProperty pushTextProperty() {
         return pushText;
     }
+    public final String getPushText() {
+        return pushText.get();
+    }
+    public final void setPushText(final String text) {
+        pushText.set(text);
+    }
+
     public ObjectProperty<ObservableList<String>> stackTableProperty() {
         return stackTable;
     }
-    public Boolean isPopButtonDisabled() {
-        return isEmptyProperty().get();
+    public final ObservableList<String> getStackTable() {
+        return stackTable.get();
     }
+
     public BooleanProperty isEmptyProperty() {
         return isEmpty;
     }
     public final Boolean getIsEmpty() {
         return isEmpty.get();
+    }
+    public Boolean isPopButtonDisabled() {
+        return getIsEmpty();
     }
 
     public ViewModel() {
@@ -50,9 +63,16 @@ public class ViewModel {
         stack.push(pushText.get());
     }
 
+    public void pop() {
+        if (isPopButtonDisabled()) {
+            return;
+        }
+        stack.pop();
+    }
+
     private void updateProperties() {
         isEmpty.set(stack.isEmpty());
-        top.set(isEmpty.get() ? "" : stack.top());
+        top.set(getIsEmpty() ? "" : stack.top());
         stackTable.set(FXCollections.observableList(stack.toList()));
     }
 }

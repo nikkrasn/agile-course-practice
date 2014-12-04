fc76efbfcbf8f1d21d364a03f3387f1b7683892d
bobylkov-f <bobylkov.f@gmail.com>
2014-12-05 00:12:10 +0300

    Update Stack.java

--- code/bobylkov-fedor/View/src/main/java/ru/unn/agile/Stack/View/Stack.java
+++ code/bobylkov-fedor/View/src/main/java/ru/unn/agile/Stack/View/Stack.java
@@ -1,9 +1,12 @@
 package ru.unn.agile.Stack.View;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.util.Callback;
 import ru.unn.agile.Stack.ViewModel.ViewModel;
 
 public class Stack {
@@ -14,25 +17,39 @@ public class Stack {
     @FXML
     private Button pushButton;
     @FXML
-    private Label topLabel;
+    private TableView<String> stackTable;
     @FXML
-    private TableView stackTable;
-    @FXML
-    private TableColumn valueColumn;
+    private TableColumn<String, String> valueColumn;
     @FXML
     private TextField pushText;
 
     @FXML
     void initialize() {
         pushText.textProperty().bindBidirectional(viewModel.pushTextProperty());
-        //stackTable.itemsProperty().bindBidirectional(viewModel.stackTableProperty());
-        //popButton.disableProperty().bindBidirectional(viewModel.isEmptyProperty());
-        //topLabel.textProperty().bindBidirectional(viewModel.topProperty());
+
+        valueColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<String, String>,
+                ObservableValue<String>>()
+        {
+            @Override
+            public ObservableValue<String> call(
+                    final TableColumn.CellDataFeatures<String, String> param) {
+                return new SimpleStringProperty(param.getValue());
+            }
+        });
+
+        stackTable.itemsProperty().bindBidirectional(viewModel.stackTableProperty());
 
         pushButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(final ActionEvent event) {
                 viewModel.push();
+            }
+        });
+
+        popButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent event) {
+                viewModel.pop();
             }
         });
     }

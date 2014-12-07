5941b47b988b240ef5349d070d3da766458a4ddc
Daniil Osokin <daniil.osokin@gmail.com>
2014-12-07 12:36:35 +0300

    Removed copy-paste friends

---
+++ code/Sokolov-Mihail/View/src/main/java/ru/unn/agile/AreaConverter/view/SimpleAreaConverter.java
@@ -0,0 +1,83 @@
+package ru.unn.agile.AreaConverter.view;
+
+import ru.unn.agile.AreaConverter.Model.AreaConverter.Measures;
+import ru.unn.agile.AreaConverter.viewmodel.ViewModel;
+import javax.swing.*;
+import java.awt.event.*;
+
+public final class SimpleAreaConverter {
+    private JTextField tbValueA;
+    private JComboBox<Measures> cbFromA;
+    private JComboBox<Measures> cbToA;
+    private JTextField tbResultA;
+    private JButton btnConvertA;
+    private JPanel mainPanelA;
+    private ViewModel viewModelA;
+
+    public static void main(final String[] args) {
+        JFrame frame = new JFrame("Simple area converter");
+        frame.setContentPane(new SimpleAreaConverter(new ViewModel()).mainPanelA);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private  SimpleAreaConverter() { }
+
+    private SimpleAreaConverter(final ViewModel viewModel) {
+        this.viewModelA = viewModel;
+        backBindAreaConverter();
+
+        loadListAreaMeasures();
+
+        btnConvertA.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent actionEvent) {
+                bindAreaConverter();
+                viewModel.convert();
+                backBindAreaConverter();
+            }
+        });
+
+        cbFromA.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent actionEvent) {
+                bindAreaConverter();
+                backBindAreaConverter();
+            }
+        });
+        cbToA.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent actionEvent) {
+                bindAreaConverter();
+                backBindAreaConverter();
+            }
+        });
+
+        KeyAdapter keyListener = new KeyAdapter() {
+            public void keyReleased(final KeyEvent e) {
+                bindAreaConverter();
+                viewModel.processKeyInTextField();
+                backBindAreaConverter();
+            }
+        };
+        tbValueA.addKeyListener(keyListener);
+    }
+
+    private void loadListAreaMeasures() {
+        Measures[] measures = Measures.values();
+        cbFromA.setModel(new JComboBox<Measures>(measures).getModel());
+        cbToA.setModel(new JComboBox<Measures>(measures).getModel());
+    }
+
+    private void bindAreaConverter() {
+        viewModelA.setInput(tbValueA.getText());
+        viewModelA.setMeasureOfAreaFrom((Measures) cbFromA.getSelectedItem());
+        viewModelA.setMeasureOfAreaTo((Measures) cbToA.getSelectedItem());
+    }
+
+    private void backBindAreaConverter() {
+        btnConvertA.setEnabled(viewModelA.isCalculateButtonEnabled());
+        tbResultA.setText(viewModelA.getResult());
+    }
+}
---
+++ code/Zotov-Alexey/Model/src/main/java/ru/unn/agile/LengthConverter/Model/LengthConverter.java
@@ -0,0 +1,53 @@
+package ru.unn.agile.LengthConverter.Model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class LengthConverter {
+
+    private static final double KOEF_M = 1.;
+    private static final double KOEF_KM = 1000.;
+    private static final double KOEF_MILE = 1609.344;
+    private static final double KOEF_INCH = 0.0254;
+
+    private static Map<Measure, Double> koef = new HashMap<Measure, Double>();
+    static {
+        koef.put(Measure.METER, KOEF_M);
+        koef.put(Measure.KILOMETER, KOEF_KM);
+        koef.put(Measure.MILE, KOEF_MILE);
+        koef.put(Measure.INCH, KOEF_INCH);
+    }
+
+    public enum Measure {
+        METER("Meter"),
+        KILOMETER("Kilometer"),
+        MILE("Mile"),
+        INCH("Inch");
+
+        public String toString() {
+            return name;
+        }
+
+        private final String name;
+
+        private Measure(final String name) {
+            this.name = name;
+        }
+    }
+
+    private LengthConverter() { }
+
+    public static double convertFromTo(final Measure from, final Measure to, final double value) {
+
+        if (koef.get(to) <= 0 || koef.get(from) <= 0) {
+            throw new IllegalArgumentException("wrong coeefs");
+        }
+        double check = koef.get(from) / koef.get(to);
+
+        if (value < 0 || Double.MAX_VALUE / check < value) {
+            throw new IllegalArgumentException("wrong input");
+        }
+
+        return value * check;
+    }
+}

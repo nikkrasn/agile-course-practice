754c9434c78dcb82c0b88e28572655ff25804346
Alexander.Myasnikov <myasnikov.alexander.s@gmail.com>
2014-12-11 00:26:37 +0300

    edited title

--- code/myasnikov-alexander/View/src/main/java/ru/unn/agile/Vector3D/view/Main.java
+++ code/myasnikov-alexander/View/src/main/java/ru/unn/agile/Vector3D/view/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(final Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
-        primaryStage.setTitle("Calculator");
+        primaryStage.setTitle("CalculatorVector3D");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }

2983d44330021004165bdff828152c498820ccfb
Evgenii Izutov <izutov.e.o@gmail.com>
2014-12-22 19:01:16 +0300

    Added LoggerToTxt class and tests.

---
+++ code/izutov-evgenii/Infrastructure/src/test/java/ru/unn/agile/DemandElasticity/infrastructure/LoggerToTxtTest.java
@@ -0,0 +1,88 @@
+package ru.unn.agile.DemandElasticity.infrastructure;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+
+import static ru.unn.agile.DemandElasticity.infrastructure.RegexMatcher.matchesPattern;
+
+public class LoggerToTxtTest {
+    private static final String FILE_NAME = "./LoggerToTxt_Tests.log";
+    private LoggerToTxt loggerToTxt;
+
+    @Before
+    public void setUp() {
+        loggerToTxt = new LoggerToTxt(FILE_NAME);
+    }
+
+    @After
+    public void cleanUp() {
+        loggerToTxt = null;
+    }
+
+    @Test
+    public void canCreateLoggerWithCorrectFileName() {
+        assertNotNull(loggerToTxt);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorThrowsExceptionOnNullFilName() {
+        new LoggerToTxt(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorThrowsExceptionOnEmptyFilName() {
+        new LoggerToTxt("");
+    }
+
+    @Test
+    public void canCreateFileForLoggingOnDisk() {
+        try {
+            new BufferedReader(new FileReader(FILE_NAME));
+        } catch (FileNotFoundException e) {
+            fail("File " + FILE_NAME + " wasn't found!");
+        }
+    }
+
+    @Test
+    public void canWriteLogMessage() {
+        String testMessage = "Test message";
+
+        loggerToTxt.logMessage(testMessage);
+
+        String message = loggerToTxt.getFullLog().get(0);
+        assertThat(message, matchesPattern(".*" + testMessage + "$"));
+    }
+
+    @Test
+    public void canWriteSeveralLogMessage() {
+        String[] messages = {"Test message #1", "Test message #2"};
+
+        loggerToTxt.logMessage(messages[0]);
+        loggerToTxt.logMessage(messages[1]);
+
+        List<String> actualMessages = loggerToTxt.getFullLog();
+        for (int i = 0; i < actualMessages.size(); i++) {
+            assertThat(actualMessages.get(i), matchesPattern(".*" + messages[i] + "$"));
+        }
+    }
+
+    @Test
+    public void doesLogContainDateAndTime() {
+        String testMessage = "Test message";
+
+        loggerToTxt.logMessage(testMessage);
+
+        String message = loggerToTxt.getFullLog().get(0);
+        assertThat(message, matchesPattern("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} > .*"));
+    }
+}

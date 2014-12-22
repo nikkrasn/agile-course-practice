b7961841b232a25b0e53027a4a25562f4cd8655b
sofi-klyan <sofi.kljan@gmail.com>
2014-12-23 01:18:44 +0300

    Added lines between some functions

--- code/klyan-sofiya/ViewModel/src/main/java/ru/unn/agile/QuickSort/viewmodel/ViewModel.java
+++ code/klyan-sofiya/ViewModel/src/main/java/ru/unn/agile/QuickSort/viewmodel/ViewModel.java
@@ -184,9 +184,11 @@ public class ViewModel {
             status.set(getInputStatus().toString());
             curVal = newVal;
         }
+        
         public boolean isChanged() {
             return !prevVal.equals(curVal);
         }
+
         public void cache() {
             prevVal = curVal;
         }
